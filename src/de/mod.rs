144d066151c1f5476bc47bebb84d8c2b5d944608
Cherry <cherry@sparklet.org>
2023-09-20 18:57:38 -0400

    add serializer boilerplate

---
+++ src/de/mod.rs
@@ -0,0 +1,7 @@
+// TODO
+
+pub fn from_str(_input: &str) {
+    todo!()
+}
+
+pub struct Deserializer {}
--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,3 @@ mod ser;
 pub use de::{from_str, Deserializer};
 pub use error::{Error, Result};
 pub use ser::{to_string, Serializer};
-
-// pub fn add(left: usize, right: usize) -> usize {
-//     left + right
-// }
-//
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
