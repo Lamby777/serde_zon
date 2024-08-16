d3880ea94c3a57ab79fd7c7e0d10906680dcc4bd
Cherry <cherry@sparklet.org>
2024-08-16 11:22:11 -0400

    copy over the example

--- src/error.rs
+++ src/error.rs
@@ -5,9 +5,23 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug)]
 pub enum Error {
-    Message(String),
-
+    TrailingChars,
     EOF,
+    ExpectedNull,
+    ExpectedBool,
+    ExpectedInt,
+    ExpectedString,
+    ExpectedArray,
+    ExpectedArrayComma,
+    ExpectedArrayEnd,
+    ExpectedMap,
+    ExpectedMapComma,
+    ExpectedMapColon,
+    ExpectedMapEnd,
+    ExpectedEnum,
+    Syntax,
+
+    Message(String),
 }
 
 impl ser::Error for Error {
@@ -24,10 +38,25 @@ impl de::Error for Error {
 
 impl Display for Error {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Error::Message(msg) => formatter.write_str(msg),
-            Error::EOF => formatter.write_str("unexpected end of input"),
-        }
+        formatter.write_str(match self {
+            Error::EOF => "unexpected end of input",
+            Error::TrailingChars => "trailing characters",
+            Error::ExpectedNull => "expected null",
+            Error::ExpectedBool => "expected boolean",
+            Error::ExpectedInt => "expected integer",
+            Error::ExpectedString => "expected string",
+            Error::ExpectedArray => "expected array",
+            Error::ExpectedArrayComma => "expected array comma",
+            Error::ExpectedArrayEnd => "expected array end",
+            Error::ExpectedMap => "expected map",
+            Error::ExpectedMapComma => "expected map comma",
+            Error::ExpectedMapColon => "expected map colon",
+            Error::ExpectedMapEnd => "expected map end",
+            Error::ExpectedEnum => "expected enum",
+            Error::Syntax => "syntax error",
+
+            Error::Message(msg) => msg,
+        })
     }
 }
 
